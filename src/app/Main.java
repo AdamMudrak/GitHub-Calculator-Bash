7f5f5e04a7059ae11ce5cab3dafc7c393232a939
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 14:13:38 +0200

    Add description

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,6 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Random text");
+        System.out.println("This app helps me learn Git Bash");
     }
 }
