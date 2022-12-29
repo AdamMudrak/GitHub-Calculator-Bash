bf424018d90d5c9b5b8b49de0d42b0ad119a10a8
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 14:11:40 +0200

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Random text");
+        System.out.println("App version 0.1");
     }
     public static double multiply(double a, double b) {
         return a * b;
